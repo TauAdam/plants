686d7384b195c4c059c54b9b455a0e556d97e14e
Miras <miamib531@gmail.com>
2023-01-26 23:06:39 +0600

    feat: add burger menu

--- plants/src/main.ts
+++ plants/src/main.ts
@@ -1,2 +1,19 @@
 import './index.scss';
 
+const burger = <HTMLButtonElement>document.querySelector('.burger-menu');
+const popup = document.querySelector('.popup');
+const navBar = <HTMLDivElement>(
+  document.querySelector('.header__list')?.cloneNode(true)
+);
+
+burger.onclick = e => {
+  e.preventDefault();
+  popup?.classList.toggle('active');
+  burger.classList.toggle('active');
+  popup?.appendChild(navBar);
+};
+const liArray = <HTMLUListElement>document.querySelector('.header__list');
+liArray.onclick = () => {
+  popup?.classList.remove('active');
+  burger?.classList.remove('active');
+};
